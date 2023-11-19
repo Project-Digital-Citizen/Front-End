59848287a2efc200c3ffa7ccc25ea4a29e0b6c11
yoelsteadyv <yoelsteadyv@gmail.com>
2023-11-19 18:33:20 +0700

    Digital Citizen

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Digital Citizen</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
