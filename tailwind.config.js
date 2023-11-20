28dd73f5d8f48133c8de4feb8005fe6129ecf0dc
yoelsteadyv <yoelsteadyv@gmail.com>
2023-11-20 12:10:44 +0700

    flowbite install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
-
