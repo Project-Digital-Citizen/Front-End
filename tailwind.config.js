3d121776c751f9854a9a40a3547e2d1d48c06ed8
yoelsteadyv <yoelsteadyv@gmail.com>
2023-11-21 16:05:23 +0700

    remove flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,10 @@
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
