a8d4d2fcf0211a07cb4b051a6d601a3496fec022
yoelsteadyv <yoelsteadyv@gmail.com>
2023-11-28 00:31:31 +0700

    page profile update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import person from "../assets/images/person.png";
 
 const Profile = () => {
   return (
@@ -8,17 +9,17 @@ const Profile = () => {
       <div className="bg-background flex items-center h-screen justify-center py-10">
         <div className="bg-white w-5/6 rounded shadow-md y-10 md:w-[70%] lg:w-[50%] xl:w-[35%]">
           {/* Isi kotak center di sini */}
-          <div
-            className="flex-col border-2 border-black justify-center"
-          >
-            
-            <div className="flex avatar placeholder my-auto border-2 border-black justify-center upload-file">
-              <label htmlFor="pp" className="bg-neutral text-neutral-content rounded-full w-24 h-24">
-                <img src="../assets/images/person.png" alt="" />
+          <div className="flex-colk justify-center">
+            <div className="flex avatar my-auto justify-center py-6">
+              <label
+                htmlFor="pp"
+                className="bg-neutral text-neutral-content rounded-full w-24 h-24"
+              >
+                <img src={person} alt="" />
               </label>
-              <input type="file" id="pp" className="uploadpp hidden"/>
+              <input type="file" id="pp" className="hidden" />
             </div>
-            <div className="border-2 border-black flex flex-col">
+            <div className="flex flex-col pb-2">
               <h1 className="text-black text-2xl font-black text-center">
                 Welcome to Digital
               </h1>
@@ -49,22 +50,9 @@ const Profile = () => {
                   className="input input-bordered input-md w-full max-w-screen md:max-w-xs"
                 />
               </div>
-              <div className="flex flex-row-reverse pt-2">
-                <a className="link ">Lupa Password</a>
-              </div>
-              <div className="pt-4 pb-6">
-                <button className="btn btn-block bg-indigo text-white ">
-                  Log In
-                </button>
-                <span className="text-xs -mt-[3px] ml-[2px] text-black">
-                  belum memiliki akun?
-                  <a
-                    className="link link-primary"
-                    onClick={() => navigate("/register")}
-                  >
-                    Register
-                  </a>
-                </span>
+              <div className="flex pt-4 pb-6 gap-3">
+                <button className="btn bg-indigo text-white ">Save</button>
+                <button className="btn btn-error text-white ">Sign Out</button>
               </div>
             </form>
           </div>
