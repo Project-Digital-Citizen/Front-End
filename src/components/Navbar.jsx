7dcbfc4861ba6c2295f73d9d50c94aadcacd65a6
yoelsteadyv <yoelsteadyv@gmail.com>
2023-12-08 15:01:25 +0700

    bug navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,61 +8,253 @@ const Navbar = () => {
   const handleUserLog = () => {
     if (cookies.get("userLog")) {
       return (
-        <div className="hidden dropdown dropdown-end md:block pr-9">
-          <i className="px-3">{cookies.get("userData").user.nama}</i>
-          <label tabIndex={0} className="">
-            <div className="m-1 avatar placeholder btn btn-circle">
-              <div className="w-12 rounded-full bg-neutral text-neutral-content">
-                <span>MX</span>
+        <>
+          <div className="hidden dropdown dropdown-end md:block pr-9">
+            <i className="px-3">{cookies.get("userData").user.nama}</i>
+            <label tabIndex={0} className="">
+              <div className="m-1 avatar placeholder btn btn-circle">
+                <div className="w-12 rounded-full bg-neutral text-neutral-content">
+                  <span>MX</span>
+                </div>
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a
+                  className="justify-between"
+                  onClick={() => navigate("/profile")}
+                >
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    cookies.remove("userLog");
+                    cookies.remove("userData");
+                    navigate("/login");
+                  }}
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className=" md:hidden">
+            <div className=" drawer drawer-end md:hidden">
+              <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+              <div className=" drawer-content">
+                {/* Page content here */}
+                <label
+                  htmlFor="my-drawer"
+                  className="btn btn-ghost drawer-button"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-6 h-6 stroke-current "
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </label>
+              </div>
+              <div className="z-50 drawer-side">
+                <label
+                  htmlFor="my-drawer"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="min-h-full gap-4 p-6 menu menu-vertical w-58 bg-base-200">
+                  {/* Sidebar content here */}
+                  <li>
+                    <details className="w-auto ">
+                      <summary className="p-2">
+                        <div className="avatar placeholder">
+                          <div className="w-12 rounded-full bg-neutral text-neutral-content">
+                            <span>{cookies.get("userData").user.nama}</span>
+                          </div>
+                        </div>
+                        <i>{cookies.get("userData").user.nama}</i>
+                      </summary>
+                      <ul>
+                        <li>
+                          <a
+                            className="p-2 hover:border border-indigo"
+                            onClick={() => navigate("/profile")}
+                          >
+                            Profile
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            className="p-2 hover:border border-indigo"
+                            onClick={() => navigate("/login")}
+                          >
+                            Logout
+                          </a>
+                        </li>
+                      </ul>
+                    </details>
+                  </li>
+                  <ul className="w-56 menu bg-base-200 rounded-box">
+                    <li>
+                      <a onClick={() => navigate("/")}>Beranda</a>
+                    </li>
+                    <li>
+                      <details>
+                        <summary>Input Pengajuan</summary>
+                        <ul>
+                          <li>
+                            <a onClick={() => navigate("/formktp")}>KTP</a>
+                          </li>
+                          <li>
+                            <a onClick={() => navigate("/formdomisili")}>
+                              Surat Domisili
+                            </a>
+                          </li>
+                        </ul>
+                      </details>
+                    </li>
+                    <li>
+                      <a onClick={() => navigate("/statuspengajuan")}>
+                        Status Pengajuan
+                      </a>
+                    </li>
+                  </ul>
+                </ul>
               </div>
             </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a
-                className="justify-between"
-                onClick={() => navigate("/profile")}
-              >
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  cookies.remove("userLog");
-                  cookies.remove("userData");
-                  navigate("/login");
-                }}
-              >
-                Logout
-              </a>
-            </li>
-          </ul>
-        </div>
+          </div>
+        </>
       );
     } else {
       return (
-        <div className="hidden gap-2 mr-10 md:flex">
-          <button
-            className="mx-auto bg-white text-indigo btn btn-sm border-indigo hover:bg-indigo hover:text-white hover:border-2 hover:border-indigo"
-            onClick={() => navigate("/login")}
-          >
-            Masuk
-          </button>
-          <button
-            className="mx-auto text-white btn btn-sm bg-indigo border-indigo hover:bg-white hover:text-indigo hover:border-2 hover:border-indigo"
-            onClick={() => navigate("/register")}
-          >
-            Daftar
-          </button>
-        </div>
+        <>
+          <div>
+            <div className="hidden gap-2 mr-10 md:flex ">
+              <button
+                className="mx-auto bg-white text-indigo btn btn-sm border-indigo hover:bg-indigo hover:text-white hover:border-2 hover:border-indigo"
+                onClick={() => navigate("/login")}
+              >
+                Masuk
+              </button>
+              <button
+                className="mx-auto text-white btn btn-sm bg-indigo border-indigo hover:bg-white hover:text-indigo hover:border-2 hover:border-indigo"
+                onClick={() => navigate("/register")}
+              >
+                Daftar
+              </button>
+            </div>
+            <div className="md:hidden">
+              <div className=" drawer drawer-end md:hidden">
+                <input
+                  id="my-drawer"
+                  type="checkbox"
+                  className="drawer-toggle"
+                />
+                <div className=" drawer-content">
+                  {/* Page content here */}
+                  <label
+                    htmlFor="my-drawer"
+                    className="btn btn-ghost drawer-button"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-6 h-6 stroke-current "
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      ></path>
+                    </svg>
+                  </label>
+                </div>
+                <div className="z-50 drawer-side">
+                  <label
+                    htmlFor="my-drawer"
+                    aria-label="close sidebar"
+                    className="drawer-overlay"
+                  ></label>
+                  <ul className="min-h-full gap-4 p-6 menu menu-vertical w-58 bg-base-200">
+                    {/* Sidebar content here */}
+                    <li>
+                      <details className="w-auto ">
+                        <summary className="p-2">
+                          <div className="avatar placeholder">
+                            <div className="w-12 rounded-full bg-neutral text-neutral-content">
+                              <span>U</span>
+                            </div>
+                          </div>
+                          <i>guest</i>
+                        </summary>
+                        <ul>
+                          <li>
+                            <a
+                              className="p-2 hover:border border-indigo"
+                              onClick={() => navigate("/profile")}
+                            >
+                              Profile
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              className="p-2 hover:border border-indigo"
+                              onClick={() => navigate("/login")}
+                            >
+                              Logout
+                            </a>
+                          </li>
+                        </ul>
+                      </details>
+                    </li>
+                    <ul className="w-56 menu bg-base-200 rounded-box">
+                      <li>
+                        <a onClick={() => navigate("/")}>Beranda</a>
+                      </li>
+                      <li>
+                        <details>
+                          <summary>Input Pengajuan</summary>
+                          <ul>
+                            <li>
+                              <a onClick={() => navigate("/formktp")}>KTP</a>
+                            </li>
+                            <li>
+                              <a onClick={() => navigate("/formdomisili")}>
+                                Surat Domisili
+                              </a>
+                            </li>
+                          </ul>
+                        </details>
+                      </li>
+                      <li>
+                        <a onClick={() => navigate("/statuspengajuan")}>
+                          Status Pengajuan
+                        </a>
+                      </li>
+                    </ul>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       );
     }
   };
@@ -127,97 +319,6 @@ const Navbar = () => {
         </div>
         {/*  */}
         {handleUserLog()}{" "}
-        <div className=" md:hidden">
-          <div className=" drawer drawer-end md:hidden">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-            <div className=" drawer-content">
-              {/* Page content here */}
-              <label
-                htmlFor="my-drawer"
-                className="btn btn-ghost drawer-button"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-6 h-6 stroke-current "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
-            </div>
-            <div className="z-50 drawer-side">
-              <label
-                htmlFor="my-drawer"
-                aria-label="close sidebar"
-                className="drawer-overlay"
-              ></label>
-              <ul className="min-h-full gap-4 p-6 menu menu-vertical w-58 bg-base-200">
-                {/* Sidebar content here */}
-                <li>
-                  <details className="w-auto ">
-                    <summary className="p-2">
-                      <div className="avatar placeholder">
-                        <div className="w-12 rounded-full bg-neutral text-neutral-content">
-                          <span>MX</span>
-                        </div>
-                      </div>
-                      <i>Username</i>
-                    </summary>
-                    <ul>
-                      <li>
-                        <a
-                          className="p-2 hover:border border-indigo"
-                          onClick={() => navigate("/profile")}
-                        >
-                          Profile
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="p-2 hover:border border-indigo"
-                          onClick={() => navigate("/login")}
-                        >
-                          Logout
-                        </a>
-                      </li>
-                    </ul>
-                  </details>
-                </li>
-                <ul className="w-56 menu bg-base-200 rounded-box">
-                  <li>
-                    <a onClick={() => navigate("/")}>Beranda</a>
-                  </li>
-                  <li>
-                    <details>
-                      <summary>Input Pengajuan</summary>
-                      <ul>
-                        <li>
-                          <a onClick={() => navigate("/formktp")}>KTP</a>
-                        </li>
-                        <li>
-                          <a onClick={() => navigate("/formdomisili")}>
-                            Surat Domisili
-                          </a>
-                        </li>
-                      </ul>
-                    </details>
-                  </li>
-                  <li>
-                    <a onClick={() => navigate("/statuspengajuan")}>
-                      Status Pengajuan
-                    </a>
-                  </li>
-                </ul>
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
 
       {/* oldnav */}
