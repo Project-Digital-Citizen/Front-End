4174bd4d20083e0839971329cb28aa5b32a09497
yoelsteadyv <yoelsteadyv@gmail.com>
2023-12-15 16:53:53 +0700

    modal bug

--- src/pages/Admin/Account.jsx
+++ src/pages/Admin/Account.jsx
@@ -3,8 +3,28 @@ import NavbarADM from "../../components/NavbarADM";
 import Footer from "../../components/Footer";
 import { useEffect, useState } from "react";
 import { userAPI } from "../../data/api-digzen";
+import EditIcon from "@mui/icons-material/Edit";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { TextField } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 3,
+};
 
 const RenderList = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const navigate = useNavigate();
   // eslint-disable-next-line react/prop-types
   const data = props?.dataUser?.data?.users;
@@ -41,6 +61,78 @@ const RenderList = (props) => {
                 >
                   Delete
                 </span>
+                <span>
+                  <i className="" onClick={handleOpen}>
+                    <EditIcon fontSize="small" />
+                  </i>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        Edit
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+                        <div className="justify-between w-full mt-2 form-control md:flex md:flex-row">
+                          <TextField
+                            id="outlined-basic"
+                            label="Nama"
+                            type="text"
+                            placeholder="Nama Lengkap"
+                            variant="outlined"
+                            className="w-full"
+                            // onBlur={(e) => handleFormValueBlur(e, "password")}
+                          />
+                        </div>
+                        <div className="justify-between w-full mt-2 form-control md:flex md:flex-row">
+                          <TextField
+                            id="outlined-basic"
+                            label="Email"
+                            type="email"
+                            placeholder="contoh@contoh.com"
+                            variant="outlined"
+                            className="w-full"
+                            // onBlur={(e) => handleFormValueBlur(e, "password")}
+                          />
+                        </div>
+                        <div className="justify-between w-full mt-2 form-control md:flex md:flex-row">
+                          <TextField
+                            id="outlined-basic"
+                            label="NIK"
+                            type="number"
+                            placeholder="xxxxxxxxx"
+                            variant="outlined"
+                            className="w-full"
+                            // onBlur={(e) => handleFormValueBlur(e, "password")}
+                          />
+                        </div>
+                        <div className="justify-between w-full mt-2 form-control md:flex md:flex-row">
+                          <TextField
+                            id="outlined-basic"
+                            label="Nomor"
+                            type="number"
+                            placeholder="08xxxxxxxxxx"
+                            variant="outlined"
+                            className="w-full"
+                            // onBlur={(e) => handleFormValueBlur(e, "password")}
+                          />
+                        </div>
+                        <div className="flex flex-row-reverse my-2">
+                          <button className="text-white btn bg-indigo hover:bg-white hover:text-indigo hover:border-1 hover:border-indigo">
+                            Save
+                          </button>
+                        </div>
+                      </Typography>
+                    </Box>
+                  </Modal>
+                </span>
               </td>
             </tr>
           </>
@@ -69,6 +161,7 @@ const Account = () => {
   return (
     <>
       <NavbarADM />
+
       <div className="flex items-center justify-center h-screen py-10 bg-background">
         <div className="bg-white w-5/6 rounded shadow-md y-10 md:w-[80%] lg:w-[60%] xl:w-[65%] relative">
           {/* Isi kotak center di sini */}
